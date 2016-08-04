7de108f876b401f729680d16900f38220c31f4ac
Lavare <yangcaimu@gmail.com>
2016-08-04 11:01:40 +0800

    Initial Commit

---
+++ UseAccessorMethodsInInitializerMethodsAndDeallocDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseAccessorMethodsInInitializerMethodsAndDeallocDemo
+//
+//  Created by Lavare on 16/8/4.
+//  Copyright © 2016年 Lavare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UseAccessorMethodsInInitializerMethodsAndDeallocDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UseAccessorMethodsInInitializerMethodsAndDeallocDemo
+//
+//  Created by Lavare on 16/8/4.
+//  Copyright © 2016年 Lavare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
